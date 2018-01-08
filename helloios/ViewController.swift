dc2fce5c7f95753b82a1cb2c37795e4176126e8d
Christy Lu <hyl9@uw.edu>
2018-01-07 16:56:43 -0800

    Initial Commit

---
+++ helloios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Christy Lu on 1/7/18.
+//  Copyright © 2018 Christy Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
